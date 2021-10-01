b0e9cb128182f7a8351d5ae74b5a32fce26c0d36
ariel <ariel.viera@gmail.com>
2021-10-01 18:23:28 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Akka.DependencyInjection.Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
